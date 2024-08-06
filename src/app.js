faab6fe49fe58e109c0307ed90ee1f2af9abecef
huzaifaAbbas821 <huzaifaabbas8121@gmail.com>
2024-08-07 01:06:42 +0500

    fixing issues 1

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors"
+import { env } from 'node:process';
 const  app = express();
 
 app.use(cookieParser());
 app.use(cors({
-    origin:"*",
+    origin:process.env.CORS_ORIGIN,
     credentials: true
 }))
 app.use(express.json({limit:"16kb"}));
