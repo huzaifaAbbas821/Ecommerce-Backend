f374757434861d3fd1890604c7008b1ed9d04834
huzaifaAbbas821 <huzaifaabbas8121@gmail.com>
2024-08-24 22:13:10 +0500

    fixing uploading issue

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -13,13 +13,6 @@
 // export const upload = multer({storage : storage})
 import multer from 'multer';
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, './public/temp');
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname); // You can customize the filename if needed
-  },
-});
+const storage = multer.memoryStorage();
 
 export const upload = multer({ storage });
